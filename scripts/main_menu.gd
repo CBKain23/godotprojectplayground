e41d2a5fbcc85941aada950a5aff000174a21531
CBKain <sterlingford5@gmail.com>
2026-02-08 12:25:01 -0600

    Added a Build

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,9 +4,18 @@ class_name MainMenu extends CanvasLayer
 @export var manager : Game_Manager
 @export var scene : PackedScene
 
+@export var month : int 
+@export var day : int
+@export var year : int = 2026
+
+@onready var build : Label = %BuildDate
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	scene.instantiate()
+	
+	build.text = "Build Date: " + str(month) + "/" + str(day) + "/" + str(year)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
