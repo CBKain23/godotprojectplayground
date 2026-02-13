4224332bf9b6881d580bd247357c175726620617
CBKain <sterlingford5@gmail.com>
2026-02-12 20:57:40 -0600

    New Build
    - Shorten timeout for the end flag object
    - Turn off the collision for the end flag after completing the level

--- scripts/end_game_flag.gd
+++ scripts/end_game_flag.gd
@@ -1,19 +1,20 @@
 class_name End_Game extends Area2D
 
+@onready var collision : CollisionShape2D = %Collision
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func endLevel(player : Character):
-	#%Collision.disabled = true
+	collision.disabled = true
 	player.game_manager.sound.stopMusic()
 	player.get_node("%P_HUD").changeWinTextVisibility()
 	player.game_manager.sound.playSFX(player.game_manager.sound.win)
 	player.game_manager.sound.playSFX(player.game_manager.sound.winJingle)
 	#await get_tree().creater_timer(0.5).timeout
 	player.game_manager.sound.playSFX(player.game_manager.sound.youWin)
-	await get_tree().create_timer(5.5).timeout
+	await get_tree().create_timer(5.0).timeout
 	get_tree().quit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,7 +25,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Character) -> void:
 	
 	#endLevel(body.game_manager)
-	endLevel(body)
+	if collision.disabled == false:
+		endLevel(body)
 
 
 func _on_delay_timeout() -> void:
